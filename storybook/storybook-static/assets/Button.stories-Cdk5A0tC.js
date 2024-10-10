52809477014be3410c4cf7d0df2626f65ada9124
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2024-10-11 01:11:41 +0800

    build

---
+++ storybook/storybook-static/assets/Button.stories-Cdk5A0tC.js
@@ -0,0 +1,21 @@
+import{f as b}from"./index-DcZGqPg1.js";import{B as y}from"./Button-Bf7kBmkp.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const k={title:"Example/Button",component:y,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:b()}},r={args:{primary:!0,label:"Button",size:"small"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button"}};var o,t,n;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button',
+    size: "small"
+  }
+}`,...(n=(t=r.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};var l,c,m;a.parameters={...a.parameters,docs:{...(l=a.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};var p,u,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,g,B;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(B=(g=s.parameters)==null?void 0:g.docs)==null?void 0:B.source}}};const C=["Primary","Secondary","Large","Small"];export{e as Large,r as Primary,a as Secondary,s as Small,C as __namedExportsOrder,k as default};
